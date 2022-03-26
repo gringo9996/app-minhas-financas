925ab9abee3e446551ee201948bd9d6f52bab243
Erick Santos <erickhenrique321@gmail.com>
2022-03-25 22:35:36 -0300

    Navbar e Navbar Item components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "bootswatch/dist/flatly/bootstrap.css";
 import "./custom.css";
 import Routes from "./appRoutes";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="container">
-      <Routes />
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/navbarItem.js
@@ -0,0 +1,9 @@
+export default function NavbarItem(props) {
+  return (
+    <li className="nav-item">
+      <a className="nav-link" href={props?.href}>
+        {props?.label}
+      </a>
+    </li>
+  );
+}
